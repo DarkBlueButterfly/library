1aef0b2da2a2334944321419ae55aa0ec616792e
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-07-04 20:20:53 -0600

    Add function to render book tiles
    Fixed bugs with objects in array not removing properly.
    Also added style to buttons.

--- javascript.js
+++ javascript.js
@@ -9,12 +9,7 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    // this.info = function() {
-    //     return `${this.title} by ${this.author}, `+ this.pages + `, ` + this.read;
-    // }
 }
-
-// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "true");
 
 const title = document.querySelector("#title");
 const author = document.querySelector("#author");
@@ -23,7 +18,7 @@ const read = document.querySelector("#read");
 
 const form = document.querySelector("form");
 const addBook = document.querySelector("#addBook");
-const bookRequest = document.querySelector("#bookRequest");
+const bookRequest = document.querySelector("#bookRequest"); //modal
 const cancelBtn = document.querySelector(".cancel");
 const confirmBtn = document.querySelector("#confirmBtn");
 
@@ -44,70 +39,73 @@ function addBookToLibrary(title, author, pages, read) {
 
 confirmBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    // addBookToLibrary(title, author, pages, read);
-    // bookRequest.close();
-    addBookToLibrary(title.value, author.value, pages.value, read.value)
-    libraryDisplay()
+    let addLib = addBookToLibrary(title.value, author.value, pages.value, read.value)
+    libraryDisplay(addLib)
     form.reset();
     bookRequest.close();
 })
 
 const mainDiv = document.querySelector("#main");
 
-// const cards = document.createElement("div");
-// cards.classList.add("card");
-// // cards.textContent = "Test"; 
-// const cardTitle = document.createElement("div");
-// cardTitle.classList.add("cardTitle");
-// cardTitle.textContent = `Title: `+ title.value;
-// const cardAuthor = document.createElement("div");
-// cardAuthor.classList.add("cardAuthor");
-// cardAuthor.textContent = `by ${author.value}`
-// const cardPages = document.createElement("div");
-// cardPages.classList.add("cardPages");
-// cardPages.textContent = `${pages.value} pages`
-// const cardRead = document.createElement("div");
-// cardRead.classList.add("cardRead");
-// cardRead.textContent = `Read? `
-
-// mainDiv.appendChild(cards);
-// cards.appendChild(cardTitle);
-// cards.appendChild(cardAuthor);
-// cards.appendChild(cardPages);
-// cards.appendChild(cardRead);
-
-function libraryDisplay() {
+function libraryDisplay(addLib) {
     // display on "cards" from arrays
-    // array.forEach(card => {
-    //     card = document.createElement("div");
-    //     card.classList.add("card");
-    //     card.textContent =  Object.values(card);
-    // });
-    // for (let book of myLibrary) {
-        // console.log("Title: " + book.title + " Author: " + book.author) //Test
-        // cards = document.createElement("div");
-        // cards.textContent = "Title: " + book.title + ` Author: ` + book.author;
-        const cards = document.createElement("div");
-        cards.classList.add("card");
-        // cards.textContent = "Test"; 
-        const cardTitle = document.createElement("div");
-        cardTitle.classList.add("cardTitle");
-        cardTitle.textContent = title.value;
-        const cardAuthor = document.createElement("div");
-        cardAuthor.classList.add("cardAuthor");
-        cardAuthor.textContent = `by ${author.value}`
-        const cardPages = document.createElement("div");
-        cardPages.classList.add("cardPages");
-        cardPages.textContent = `${pages.value} pages`
-        const cardRead = document.createElement("div");
-        cardRead.classList.add("cardRead");
-        cardRead.textContent = `Read? `
+    const cards = document.createElement("div");
+    cards.classList.add("card");
 
-        mainDiv.appendChild(cards);
-        cards.appendChild(cardTitle);
-        cards.appendChild(cardAuthor);
-        cards.appendChild(cardPages);
-        cards.appendChild(cardRead);
-    // }
+    const cardTitle = document.createElement("div");
+    cardTitle.classList.add("cardTitle");
+    cardTitle.textContent = title.value;
 
+    const cardAuthor = document.createElement("div");
+    cardAuthor.classList.add("cardAuthor");
+    cardAuthor.textContent = `by ${author.value}`
+
+    const cardPages = document.createElement("div");
+    cardPages.classList.add("cardPages");
+    cardPages.textContent = `${pages.value} pages`
+
+    const cardRead = document.createElement("div");
+    cardRead.classList.add("cardRead");
+    if (read.checked === true) {
+        cardRead.textContent = `Read`;
+    } else {
+        cardRead.textContent = `Not Read`;
+    }
+
+    // Delete button:
+    const deleteBook = document.createElement("button");
+    deleteBook.classList.add("delete");
+    deleteBook.textContent = "Delete";
+
+    deleteBook.addEventListener("click", () => {
+        removeBook(addLib.id);
+        cards.remove();
+    })
+
+    function removeBook(id) {
+        let indexID = myLibrary.findIndex((book) => book.id === id)
+        console.log(`Index is ${id}`) //check
+        myLibrary.splice(indexID, 1);
+    }
+
+    // Read toggle:
+    const haveRead = document.createElement("button");
+    haveRead.classList.add("haveRead");
+    haveRead.textContent = "Read Status";
+
+    haveRead.addEventListener("click", () => {
+        if (cardRead.textContent === `Read`) {
+            cardRead.textContent = `Not Read`;
+        } else {
+            cardRead.textContent = `Read`;
+        }
+    })
+
+    mainDiv.appendChild(cards);
+    cards.appendChild(cardTitle);
+    cards.appendChild(cardAuthor);
+    cards.appendChild(cardPages);
+    cards.appendChild(cardRead);
+    cards.appendChild(deleteBook);
+    cards.appendChild(haveRead);
 }
